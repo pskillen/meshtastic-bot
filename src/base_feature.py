a320eaf7d59546f66260fc84f2834f22b9c1c45e
Patrick Skillen <patrick.skillen2@nhs.scot>
2025-03-15 23:17:07 +0000

    refactor: extract base class for commands and responders

---
+++ src/base_feature.py
@@ -0,0 +1,66 @@
+import logging
+from abc import ABC
+
+from meshtastic.protobuf.mesh_pb2 import MeshPacket
+
+from src.bot import MeshtasticBot
+
+
+class AbstractBaseFeature(ABC):
+    """
+    This class represents base functionality for commands, responders, etc
+    """
+    bot: MeshtasticBot
+
+    def __init__(self, bot: MeshtasticBot):
+        self.bot = bot
+
+    def reply_in_channel(self, packet: MeshPacket, message: str, want_ack=False) -> None:
+        """
+        Reply to a message in the same channel
+        """
+        channel = packet['channel'] if 'channel' in packet else 0
+        self.message_in_channel(channel, message, want_ack)
+
+    def message_in_channel(self, channel: int, message: str, want_ack=False) -> None:
+        """
+        Send a message in a channel
+        """
+        logging.debug(f"Sending message: '{message}'")
+        self.bot.interface.sendText(message, channelIndex=channel, wantAck=want_ack)
+
+    def reply_in_dm(self, packet: MeshPacket, message: str, want_ack=False) -> None:
+        """
+        Reply in a direct message to a user
+        """
+        destination_id = packet['fromId']
+        self.message_in_dm(destination_id, message, want_ack)
+
+    def message_in_dm(self, destination_id: str, message: str, want_ack=False) -> None:
+        """
+        Reply in a direct message to a user
+        """
+        logging.debug(f"Sending DM: '{message}'")
+        self.bot.interface.sendText(message, destinationId=destination_id, wantAck=want_ack)
+
+    def react_in_channel(self, packet: MeshPacket, emoji: str) -> None:
+        """
+        Send a reaction emoji to a message
+        """
+        logging.debug(f"Reacting to message with emoji: '{emoji}'")
+
+        reply_id = packet['id']
+        channel = packet['channel'] if 'channel' in packet else 0
+
+        self.bot.interface.sendReaction(emoji, reply_id, channelIndex=channel)
+
+    def react_in_dm(self, packet: MeshPacket, emoji: str) -> None:
+        """
+        Send a reaction emoji to a message
+        """
+        logging.debug(f"Reacting to message with emoji: '{emoji}'")
+
+        reply_id = packet['id']
+        sender = packet['fromId']
+
+        self.bot.interface.sendReaction(emoji, reply_id, sender)
---
+++ test/test_base_feature.py
@@ -0,0 +1,56 @@
+import unittest
+
+from meshtastic.protobuf.mesh_pb2 import MeshPacket
+
+from src.base_feature import AbstractBaseFeature
+from test.test_setup_data import build_test_text_packet, get_test_bot
+
+
+class ConcreteBaseFeature(AbstractBaseFeature):
+    def handle_packet(self, packet: MeshPacket) -> None:
+        pass
+
+
+class TestAbstractBaseFeature(unittest.TestCase):
+    def setUp(self):
+        self.bot, self.test_non_admin_nodes, self.test_admin_nodes = get_test_bot()
+        self.feature = ConcreteBaseFeature(self.bot)
+        self.mock_interface = self.bot.interface
+
+    def test_reply_in_channel(self):
+        sender = self.test_non_admin_nodes[1]
+        packet = build_test_text_packet('!test', sender.user.id, self.bot.my_id, channel=1)
+        self.feature.reply_in_channel(packet, "Test message")
+        self.mock_interface.sendText.assert_called_once_with("Test message", channelIndex=1, wantAck=False)
+
+    def test_message_in_channel(self):
+        self.feature.message_in_channel(1, "Test message")
+        self.mock_interface.sendText.assert_called_once_with("Test message", channelIndex=1, wantAck=False)
+
+    def test_reply_in_dm(self):
+        sender = self.test_non_admin_nodes[1]
+        packet = build_test_text_packet('!test', sender.user.id, self.bot.my_id)
+        self.feature.reply_in_dm(packet, "Test message")
+        self.mock_interface.sendText.assert_called_once_with("Test message", destinationId=sender.user.id, wantAck=False)
+
+    def test_message_in_dm(self):
+        sender = self.test_non_admin_nodes[1]
+        self.feature.message_in_dm(sender.user.id, "Test message")
+        self.mock_interface.sendText.assert_called_once_with("Test message", destinationId=sender.user.id,
+                                                             wantAck=False)
+
+    def test_react_in_channel(self):
+        sender = self.test_non_admin_nodes[1]
+        packet = build_test_text_packet('!test', sender.user.id, self.bot.my_id, channel=1)
+        self.feature.react_in_channel(packet, "👍")
+        self.mock_interface.sendReaction.assert_called_once_with("👍", packet['id'], channelIndex=1)
+
+    def test_react_in_dm(self):
+        sender = self.test_non_admin_nodes[1]
+        packet = build_test_text_packet('!test', sender.user.id, self.bot.my_id)
+        self.feature.react_in_dm(packet, "👍")
+        self.mock_interface.sendReaction.assert_called_once_with("👍", packet['id'], sender.user.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
