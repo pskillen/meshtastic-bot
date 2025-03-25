dd311d68e318748e3b9f886ff25c68477dbaae8f
Patrick Skillen <patrick.skillen2@nhs.scot>
2025-03-25 12:37:08 +0000

    fix: move scripts back to correct dir, call pip install on deploy

--- scripts/deploy_rpi.sh
+++
@@ -1,4 +0,0 @@
-script_dir="$(dirname "$0")"
-project_dir="$(realpath "${script_dir}/..")"
-
-rsync -av --exclude-from="${project_dir}/.rsyncignore" "${project_dir}/" paddy@meshcontrol.local:~/MeshtasticBot
